01c58e6bb9b2f6eac7af9b6b0c60ec72eef64fbb
CraigVaz2 <119460921+CraigVaz2@users.noreply.github.com>
2026-01-11 03:17:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function checkCode() {
+  const input = document.getElementById("codeInput").value.trim();
+  const error = document.getElementById("error");
+
+  const correctCode = "1234"; // ← we will change this later
+
+  if (input === correctCode) {
+    window.location.href = "unlocked.html";
+  } else {
+    error.textContent = "Access denied.";
+  }
+}
 
